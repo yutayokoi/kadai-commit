aa1f5d71a6db1169fd2657ec327af0126d53e062
EC2 Default User <ec2-user@ip-172-31-43-167.ec2.internal>
2019-04-17 00:47:16 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,3 +1,6 @@
+<?php
+    $result = '無し';
+?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
@@ -6,7 +9,7 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：無し</p>
+        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>

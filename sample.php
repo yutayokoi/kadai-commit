664e1cfdd86f94a5027ce470fd2458830e26f2db
EC2 Default User <ec2-user@ip-172-31-43-167.ec2.internal>
2019-04-17 00:48:26 +0000

    add omikuji function

--- sample.php
+++ sample.php
@@ -1,5 +1,13 @@
 <?php
     $result = '無し';
+    if (array_key_exists('act', $_POST)) {
+        $result = omikuji();
+    }
+    
+    function omikuji() {
+        $fortune = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"];
+        return $fortune[random_int(0, count($fortune) - 1)];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
